865aa9bd85cdd822e1cce5bf23687a0fb28977c2
Dries Vints <dries@vints.io>
2023-01-20 23:31:49 +0100

    Fix tests

--- tests/Unit/CustomerTest.php
+++ tests/Unit/CustomerTest.php
@@ -8,10 +8,22 @@ use PHPUnit\Framework\TestCase;
 class CustomerTest extends TestCase
 {
     /** @test */
-    public function it_asserts_true()
+    public function it_can_determine_if_the_customer_is_on_a_generic_trial()
     {
-        $customer = new Customer(['trial_ends_at' => now()->addDays(7)]);
+        $customer = new Customer();
+        $customer->setDateFormat('Y-m-d H:i:s');
+        $customer->trial_ends_at = now()->addDays(7);
 
         $this->assertTrue($customer->onGenericTrial());
+    }
+
+    /** @test */
+    public function it_can_determine_if_the_customer_has_an_expired_generic_trial()
+    {
+        $customer = new Customer();
+        $customer->setDateFormat('Y-m-d H:i:s');
+        $customer->trial_ends_at = now()->subDays(7);
+
+        $this->assertTrue($customer->hasExpiredGenericTrial());
     }
 }
