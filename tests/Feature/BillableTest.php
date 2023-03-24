54c59947295049cd7e54a2690b33799c0ede0641
Dries Vints <dries@vints.be>
2023-03-24 22:04:52 +0100

    wip

--- tests/Feature/BillableTest.php
+++ tests/Feature/BillableTest.php
@@ -21,11 +21,11 @@ it('can generate a checkout for a billable with custom data', function () {
         ->toBe('https://lemon.lemonsqueezy.com/checkout/buy/variant_123?checkout%5Bcustom%5D%5Bbillable_id%5D=user_123&checkout%5Bcustom%5D%5Bbillable_type%5D=users&checkout%5Bcustom%5D%5Bbatch_id%5D=789');
 });
 
-it('cannnot overwrite the customer id or type for a billable', function () {
+it('cannnot overwrite the customer id and type or subscription id for a billable', function () {
     config()->set('lemon-squeezy.store', 'lemon');
 
     $this->expectExceptionMessage(
-        'You cannot use "billable_id" or "billable_type" as custom data keys because these are reserved keywords.'
+        'You cannot use "billable_id", "billable_type" or "subscription_type" as custom data keys because these are reserved keywords.'
     );
 
     (new User)->checkout('variant_123')
